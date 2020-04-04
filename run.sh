6bee24b79ff3d718c612e0f56e225dbba72b5c09
zulin <335882264@qq.com>
2020-04-04 18:02:48 +0800

    token

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 
 #!/bin/sh
 
-if [[ -z "${TOKEN}" ]]; then
+if [[ -z "${token}" ]]; then
     echo "authentication_method = token" >> /etc/frps.ini
-    echo ${TOKEN} >> /etc/frps.ini
+    echo ${token} >> /etc/frps.ini
 fi
 
 /usr/local/frps -c /etc/frps.ini
