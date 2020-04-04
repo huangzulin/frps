b4dd3979f4fbdb97d46b3de7bf46417c07e2d846
zulin <335882264@qq.com>
2020-04-04 12:54:35 +0800

    rm aliyun

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 FRP_VERSION="0.32.1"
 
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+#sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
 
 apk add --no-cache wget
 wget --no-check-certificate https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz \
